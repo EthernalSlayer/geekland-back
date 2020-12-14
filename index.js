6399a13a429178388fe426f1b8392f8d087de3c7
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-12-14 13:48:07 +0100

    add dependancies

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-'use strict'
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT;
 
 // Create the express app
-const app = express()
+const app = express();
 
 // Routes and middleware
 // app.use(/* ... */)
 // app.get(/* ... */)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Error handlers
-app.use(function fourOhFourHandler (req, res) {
-  res.status(404).send()
-})
-app.use(function fiveHundredHandler (err, req, res, next) {
-  console.error(err)
-  res.status(500).send()
-})
+app.use(function fourOhFourHandler(req, res) {
+  res.status(404).send();
+});
+app.use(function fiveHundredHandler(err, req, res, next) {
+  console.error(err);
+  res.status(500).send();
+});
 
 // Start server
-app.listen(1234, function (err) {
+app.listen(PORT, function (err) {
   if (err) {
-    return console.error(err)
+    return console.error(err);
   }
 
-  console.log('Started at http://localhost:1234')
-})
+  console.log(`Started at http://localhost:${PORT}`);
+});
